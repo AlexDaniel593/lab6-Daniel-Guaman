f17adc6f4cffc4513f9c032d6533ac3e43106ea5
AlexDaniel593 <dalexis203@gmail.com>
2026-01-22 08:55:00 -0500

    Fix factorial test case to return correct value

--- math.test.js
+++ math.test.js
@@ -12,7 +12,7 @@ describe('Factorial', () => {
   });
 
   test('factorial de 5 debe ser 120', () => {
-    expect(factorial(5)).toBe(999); // Valor incorrecto intencionalmente
+    expect(factorial(5)).toBe(120);
   });
 
   test('factorial de 10 debe ser 3628800', () => {
